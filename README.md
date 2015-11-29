3bd10a9a7d6b428a9f76de4bc9697e68f5a96b0a
Doug Wade <douglas.b.wade@gmail.com>
2015-11-28 17:21:09 -0800

    Parse dice rolls

---
+++ README.md
@@ -0,0 +1,13 @@
+## Usage
+
+A simple dungeons and dragons dice roller.
+
+## Installation
+To install:
+
+    npm install -g dnd-roller
+
+## Usage
+To use:
+
+    dnd -d "1d20 + 7"
---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var argv = require("minimist")(process.argv.slice(2));
+var dice = require("dice.js");
+
+function printUsage() {
+  console.log("A simple dice roller for use with dungeons and dragons.");
+  console.log("-h | --help print this help message");
+  console.log("-d | --dice roll a simple dice roll");
+}
+
+if (argv.d || argv.dice) {
+  var result = dice.roll(argv.d || argv.dice);
+  console.log(+result);
+} else if (argv.h || argv.help) {
+  printUsage();
+} else {
+  console.error("Error: did not recognize options: " + argv);
+  printUsage();
+}
