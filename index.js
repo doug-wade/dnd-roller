0fe484d8b3b27c6bcf71d08defab28c82997c109
Doug Wade <douglas.b.wade@gmail.com>
2015-11-30 01:01:52 -0800

    Refactor to use subcommands

--- index.js
+++ index.js
@@ -3,25 +3,16 @@
 "use strict";
 
 var argv = require("minimist")(process.argv.slice(2));
-var dice = require("dice.js");
 
-function printUsage() {
-  console.log("A simple dice roller for use with dungeons and dragons.");
-  console.log("-h | --help print this help message");
-  console.log("-d | --dice roll a simple dice roll");
-}
-
-if (argv.d || argv.dice) {
-  var result = dice.roll(argv.d || argv.dice);
-  if (result.rightSide === 1) {
-    console.log("Critical miss!");
-  } else if (result.rightSide === 20) {
-    console.log("Critical hit!");
-  }
-  console.log(+result);
-} else if (argv.h || argv.help) {
-  printUsage();
+if (argv.h || argv.help) {
+  require("./lib/help")();
 } else {
-  console.error("Error: did not recognize options: " + JSON.stringify(argv));
-  printUsage();
+  try {
+    var command = argv._[0];
+    require("./lib/" + command)(argv);
+  } catch (e) {
+    console.error(e);
+    console.error("Error: did not recognize options " + JSON.stringify(argv));
+    require("./lib/help")();
+  }
 }
