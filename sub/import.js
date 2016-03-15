53f81f0010b997613867fecfa6e9a51aca61d3f6
Snyder <bobsnyder@Roberts-MacBook-Air.local>
2016-03-14 20:51:35 -0700

    existsSync-Update
    existsSync was depreciated.  Anyone who runs an updated version of node
    when compiling will not notice that a folder wasn’t created and thus,
    it won’t be able to maintain the state of which RP system you are using.

--- sub/import.js
+++ sub/import.js
@@ -14,10 +14,26 @@ module.exports = function (argv) {
   var charactersDirectory = 'persist/characters';
   var dest = path.join(charactersDirectory, filename);
 
-  if (!fs.existsSync(charactersDirectory)) {
-    fs.mkdirSync(charactersDirectory);
-  }
-
+  /*
+  "fs.existsSync(path)" has been depreciated.  It's probably a good idea to shift
+  this to the more current 'fs.accessSync(path, fs.F_OK)'.
+  OLD CODE:
+  ---
+    if (!fs.existsSync(charactersDirectory)) {
+      fs.mkdirSync(charactersDirectory);
+    }
+  ---
+  */
+  //--- new code
+  //fs.R_OK checks to see if the file can be read and fs.W_OK checks if it can be written
+  //the previous version only
+    fs.access(charactersDirectory, fs.F_OK, function(err) {
+        // function(err) populates an error to err if there is not able to read the file.
+        if (err) {
+            fs.mkdirSync(charactersDirectory);
+        }
+  });
+  //-- end of new code
   fs
     .createReadStream(filepath)
     .pipe(fs.createWriteStream(dest));
