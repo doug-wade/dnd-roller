de15f833b576c9c9dd5e0abdeba5f6f852902c65
Snyder <bobsnyder@Roberts-MacBook-Air.local>
2016-03-15 15:09:15 -0700

    accessSync
    Changed it to accessSync as requested.

--- sub/import.js
+++ sub/import.js
@@ -14,26 +14,10 @@ module.exports = function (argv) {
   var charactersDirectory = 'persist/characters';
   var dest = path.join(charactersDirectory, filename);
 
-  /*
-  "fs.existsSync(path)" has been depreciated.  It's probably a good idea to shift
-  this to the more current 'fs.accessSync(path, fs.F_OK)'.
-  OLD CODE:
-  ---
-    if (!fs.existsSync(charactersDirectory)) {
-      fs.mkdirSync(charactersDirectory);
-    }
-  ---
-  */
-  //--- new code
-  //fs.R_OK checks to see if the file can be read and fs.W_OK checks if it can be written
-  //the previous version only
-    fs.access(charactersDirectory, fs.F_OK, function(err) {
-        // function(err) populates an error to err if there is not able to read the file.
-        if (err) {
-            fs.mkdirSync(charactersDirectory);
-        }
-  });
-  //-- end of new code
+  if (!fs.accessSync(charactersDirectory, fs.F_OK)) {
+    fs.mkdirSync(charactersDirectory);
+  }
+
   fs
     .createReadStream(filepath)
     .pipe(fs.createWriteStream(dest));
